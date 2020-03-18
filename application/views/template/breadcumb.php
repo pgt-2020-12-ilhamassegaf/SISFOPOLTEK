2d50554f87afc939f1631e304b2a4df9206b5791
pgt-2020-12-ilhamassegaf <ilhamassegaf28@gmail.com>
2020-03-18 08:32:16 +0700

    menambahkan beberapa perubahan dashboard

--- application/views/template/breadcumb.php
+++ application/views/template/breadcumb.php
@@ -15,10 +15,9 @@
                          </div>
                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                              <ul class="breadcome-menu">
-                                 <li><a href="#">Home</a> <span class="bread-slash">/</span>
+                                 <li><a href="#">Home</a> <span class="bread-slash"></span>
                                  </li>
-                                 <li><span class="bread-blod">Dashboard</span>
-                                 </li>
+
                              </ul>
                          </div>
                      </div>
