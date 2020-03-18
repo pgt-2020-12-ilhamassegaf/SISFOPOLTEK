6022625636445e57a9fde5e4b8a05f96d44a521b
pgt-2020-12-ilhamassegaf <ilhamassegaf28@gmail.com>
2020-03-18 07:52:01 +0700

    ini adalah awal project

---
+++ application/views/template/footer.php
@@ -0,0 +1,71 @@
+ </<div>
+ </div>
+ <!-- Footer Start-->
+ <div class="footer-copyright-area">
+ 	<div class="container-fluid">
+ 		<div class="row">
+ 			<div class="col-lg-12">
+ 				<div class="footer-copy-right">
+ 					<p>Copyright &#169; 2018 Colorlib All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
+ <!-- Footer End-->
+ <!-- jquery
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/vendor/jquery-1.11.3.min.js"></script>
+ <!-- bootstrap JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+ <!-- meanmenu JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/jquery.meanmenu.js"></script>
+ <!-- mCustomScrollbar JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+ <!-- sticky JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/jquery.sticky.js"></script>
+ <!-- scrollUp JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/jquery.scrollUp.min.js"></script>
+ <!-- counterup JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/counterup/jquery.counterup.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/counterup/waypoints.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/counterup/counterup-active.js"></script>
+ <!-- peity JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/peity/jquery.peity.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/peity/peity-active.js"></script>
+ <!-- sparkline JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/sparkline/jquery.sparkline.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/sparkline/sparkline-active.js"></script>
+ <!-- flot JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/flot/Chart.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/flot/flot-active.js"></script>
+ <!-- map JS
+		============================================ -->
+ <script src="j<?php echo base_url() ?>assets/s/map/raphael.min.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/map/jquery.mapael.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/map/france_departments.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/map/world_countries.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/map/usa_states.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/map/map-active.js"></script>
+ <!-- data table JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/data-table/bootstrap-table.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/tableExport.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/data-table-active.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/bootstrap-table-editable.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/bootstrap-editable.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/bootstrap-table-resizable.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/colResizable-1.5.source.js"></script>
+ <script src="<?php echo base_url() ?>assets/js/data-table/bootstrap-table-export.js"></script>
+ <!-- main JS
+		============================================ -->
+ <script src="<?php echo base_url() ?>assets/js/main.js"></script>
